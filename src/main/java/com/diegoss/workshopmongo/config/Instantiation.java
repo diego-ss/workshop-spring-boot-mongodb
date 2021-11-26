0725b6c8ebe2a3196a4d2d0bfcd91d29e338a97b
Diego <diego_sousa.s@outlook.com>
2021-11-26 12:59:38 -0300

    AuthorDTO

--- src/main/java/com/diegoss/workshopmongo/config/Instantiation.java
+++ src/main/java/com/diegoss/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.diegoss.workshopmongo.domain.Post;
 import com.diegoss.workshopmongo.domain.User;
+import com.diegoss.workshopmongo.dto.AuthorDTO;
 import com.diegoss.workshopmongo.repositories.PostRepository;
 import com.diegoss.workshopmongo.repositories.UserRepository;
 
@@ -35,10 +36,12 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para SP. Abraços!", maria);
-		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", maria);
-		
+		//para já gerar os ids
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));		
+
+		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para SP. Abraços!", new AuthorDTO(maria));
+		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(p1, p2));
 		
 	}
